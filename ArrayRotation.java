28be73d767a118c54dfc3e31c531bc6b9f987977
StilianKaridov <100438422+StilianKaridov@users.noreply.github.com>
2022-03-01 17:01:45 +0200

    TestUpdate

--- ArrayRotation.java
+++ ArrayRotation.java
@@ -4,19 +4,6 @@ import java.util.Scanner;
 
 public class ArrayRotation {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String[] arr = input.split(" ");
-        int numberRotations = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < numberRotations; i++) {
-            String temp = arr[0];
-            for (int j = 0; j < arr.length - 1; j++) {
-                arr[j] = arr[j + 1];
-            }
-            arr[arr.length - 1] = tempo;
-        }
-        
-            
-        }
+       
     }
 }
