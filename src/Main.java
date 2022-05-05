a501465a545b10e06f0c5279b0a0e3c63a850498
StilianKaridov <karidovstilian@gmail.com>
2022-05-05 12:32:31 +0300

    Tried if the book method is working properly

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,9 @@ public class Main {
             Date date = new Date("31","12","22");
             Meet meet = new Meet("12:24", "13:00", "Stilian", "To buy coffee");
             calendar.book(date, meet);
+            Date date2 = new Date("31", "12", "22");
+            Meet meet2 = new Meet("12:20", "13:23", "Ivan", "Exception");
+            calendar.book(date2, meet2);
         } catch (InvalidDateException e){
             System.out.println(e.getMessage());
         }
