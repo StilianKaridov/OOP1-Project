54b8c0dd7d6b9e23cf8238afd631557398c01a06
StilianKaridov <karidovstilian@gmail.com>
2022-05-05 12:32:05 +0300

    Changed to extend Runtime Exception

--- src/Exceptions/InvalidDateException.java
+++ src/Exceptions/InvalidDateException.java
@@ -1,6 +1,6 @@
 package Exceptions;
 
-public class InvalidDateException extends Throwable {
+public class InvalidDateException extends RuntimeException {
     public InvalidDateException() {
         System.out.println("The date is invalid!");
     }
